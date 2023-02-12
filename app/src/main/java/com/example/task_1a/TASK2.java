7526f8a543e6d4c90fa4174b850386bca8f95dd0
shahzaib <shafiqshahzaib@gmail.com>
2023-02-12 14:35:59 +0500

    a

---
+++ app/src/main/java/com/example/task_1a/TASK2.java
@@ -0,0 +1,22 @@
+package com.example.task_1a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class TASK2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task2);
+
+
+
+    }
+}
